58cf51c37ee457cae95465ba0371d18aaddc96f1
TJ Zimmerman <tj@tjzimmerman.com>
2023-04-22 17:04:37 -0700

    feat: Add MetalLB, port Argo installer to Kustomize

--- ArgoCD/install_argocd.sh
+++
@@ -1,6 +0,0 @@
-# For some reason Kustomize isn't respecting the namespace field for the helmCharts transformer. So... Shell it is. 
-#!/bin/bash
-
-kubectl create ns argo-system
-helm repo add argo https://argoproj.github.io/argo-helm
-helm install argocd argo/argo-cd -n argo-system -f values.yml
