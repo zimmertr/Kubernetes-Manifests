383ff835b70166b9c274d7c7a6f630e3f52b2faf
TJ Zimmerman <tj@tjzimmerman.com>
2024-01-27 18:43:51 -0800

    use seed script

---
+++ misc/homepage/files/scripts/seed-volumes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REPO=$REPO
+BRANCH=main
+
+seed_configs(){
+  mkdir -p /app/config/
+  cp /config/services.yaml /app/config/services.yaml
+  cp /config/settings.yaml /app/config/settings.yaml
+}
+
+seed_icons(){
+  mkdir -p /app/public/icons
+  wget -O /app/public/icons/supermicro-ipmi.png https://raw.githubusercontent.com/$REPO/$BRANCH/misc/homepage/files/icons/supermicro-ipmi.png
+  wget -O /app/public/icons/kiali.png https://raw.githubusercontent.com/$REPO/$BRANCH/misc/homepage/files/icons/kiali.png
+}
+
+seed_images(){
+  mkdir -p /app/public/images
+  wget -O /app/public/images/background.jpg https://raw.githubusercontent.com/$REPO/$BRANCH/misc/homepage/files/images/background.jpg
+  wget -O /app/public/images/favicon.png https://raw.githubusercontent.com/$REPO/$BRANCH/misc/homepage/files/images/favicon.png
+}
+
+main(){
+  seed_configs
+  seed_icons
+  seed_images
+}
+
+main "$@"
