a2f2ee3b95419ea5046bbdbc65bf2e152410b753
TJ Zimmerman <tj@tjzimmerman.com>
2026-05-02 12:47:50 -0700

    improvements

--- misc/mountaineers-activity-scraper/files/mountaineers-activity-scraper-script.sh
+++ misc/mountaineers-activity-scraper/files/mountaineers-activity-scraper-script.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
+FILTER_ACTIVITY="Climbing"
+FILTER_TYPE="Trip"
+FILTER_CLIMBING_CATEGORY="Basic Alpine,Rock Climb"
+SHEET_NAME="Mountaineers Trips"
+
+
 timestamp() {
   date '+%Y-%m-%d %H:%M:%S'
 }
-
 log_block() {
   echo ""
   echo "----------------------------------------------------------------------"
@@ -13,19 +18,17 @@ log_block() {
 }
 
 log_block "Collecting URLs..."
-
 python3 -m mountaineers_activity_scraper.cli \
   --mode collect \
-  --filter-activity Climbing \
-  --filter-type Trip \
-  --filter-climbing-category "Basic Alpine","Rock Climb" \
+  --filter-activity "$FILTER_ACTIVITY" \
+  --filter-type "$FILTER_TYPE" \
+  --filter-climbing-category "$FILTER_CLIMBING_CATEGORY" \
   --output-filename /data/urls_climbing.txt
 
 log_block "Parsing URLs..."
-
 python3 -m mountaineers_activity_scraper.cli \
   --mode scrape \
   --input-urls-filename /data/urls_climbing.txt \
   --output-destination google-sheets \
-  --sheet "Mountaineers Trips" \
+  --sheet "$SHEET_NAME" \
   --creds /data/google_cloud_credentials.json
